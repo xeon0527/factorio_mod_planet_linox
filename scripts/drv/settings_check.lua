8042d70f84c356ec82e166cc50e77056b516cc3a
Xeon <xeon0527@naver.com>
2025-11-19 17:56:51 +0900

    영어 번역 추가

--- scripts/drv/settings_check.lua
+++ scripts/drv/settings_check.lua
@@ -2,6 +2,6 @@ local bootstrap = require("scripts.drv.bootstrap")
 
 bootstrap.create_game_start_handler(function()
   if settings.global["recursive-blueprints-area"].value ~= "center" then
-    game.print({"settings-warning"})
+    game.print({"system.settings-warning"})
   end
 end)
